41481352d2ff7a09a18bb200d3be8a0c88bbcbac
Jesus Lopez <jesus@jesusla.com>
2023-07-16 00:05:07 -0600

    first suggestions

--- api.js
+++ api.js
@@ -3,6 +3,9 @@
 const app = null;
 const db = null;
 
+// Ensure images and pdfs are available as a Buffer in req.body
+app.use(express.raw({ contentType: ["image/*", "application/pdf"]}));
+
 function sendError(res, statusCode, code, description) {
     res.status(statusCode).json({
         code: code,
@@ -11,63 +14,73 @@ function sendError(res, statusCode, code, description) {
 
 }
 
-app.get('/blobs/:blob_id', (req, res) => {
+app.get('/blobs/:blobId', (req, res) => {
     db.query('select content_type, blob_value from blobs where blob_id = ?',
-        [req.params.blob_id],
+        [req.params.blobId],
         (err, data) => {
             if (data.length != 1) {
                 sendError(res, 404, 'NotFound', 'Blob not found');
             } else {
-                res.contentType(data[0].content_type).send(data[0].blob_value);
+                const row = data[0];
+                res.set('Content-Type', row.content_type);
+                res.send(row.blob_value);
             }
     });
 });
 
-app.post('/contracts/:contract_id/cfdi', (req, res) => {
-    let { contractFile } = req.body;
+app.post('/contracts/:contractId/cfdi', (req, res) => {
+    if (!req.is("image/*") && !req.is("application/pdf")) {
+        sendError(res, 415, "UnsupportedMediaType", "CFDI tiene formato inválido")
+    }
+    const contractId = req.params.contractId;
+    const contractFile = req.body;
+    const contentType = req.get('Content-Type');
     db.query('select * from contracts where contract_id = ?',
-        [req.params.contract_id],
-        (err, data) => {
-            if (data[0] != null)
-            {
+        [contractId],
+        (err, rows) => {
+            if (rows.length != 1) {
+                sendError(res, 404, 'NotFound', 'Contract not found');
+                return;
+            }
+            const contract = rows[0];
+            if (contract.cfdi_id !== null) {
                 sendError(res, 400, 'CFDIAlreadyExists', 'El contrato ya tiene CFDI');
             } else {
                 db.query("insert into blobs (content_type, content) (?, ?)",
-                [req.headers['Content-Type'], contractFile],
-                (err, blobData) => {
+                [contentType, contractFile],
+                (err, results) => {
                     if (err) {
                         sendError(res, 500, 'InternalServerError', 'Could not insert');
                     } else {
+                        const blobId = results.insertId;
                         db.query('update contracts where contract_id = ? set cfdi_id = ?',
-                        [req.params.contract_id, blob_id],
-                        (err, data) => {
-                            if (err) {
-                                sendError(res, 500, 'InternalServerError', 'Could not update');
-                            } else {
-                                res.status(204).end;
+                            [contractId, blobId],
+                            (err) => {
+                                if (err) {
+                                    sendError(res, 500, 'InternalServerError', 'Could not update');
+                                } else {
+                                    res.status(204).end;
+                                }
                             }
-                        })
+                        )
                     }
                 })
             }
-    })
+        }
+    )
 });
 
-app.put('/contracts/:contract_id/accept', function (req, res) {
-    db.query('update contracts where contract_id = ? set accepted = \'true\'',
-        [req.params.contract_id],
-        (err, data) => {
+app.put('/contracts/:contractId/accept', function (req, res) {
+    const contractId = req.params.contractId;
+    db.query('update contracts where contract_id = ? set accepted = true',
+        [contractId],
+        (err, result) => {
             if (err) {
                 sendError(res, 500, 'InternalServerError', 'Could not update');
+            } else if (result.affectedRows == 0) {
+                sendError(res, 404, 'NotFound', 'Contract not found');
             } else {
                 res.status(204).end;
             }
     });
 });
-
-
-    // contract = select * from contracts where contract_id = :contract_id
-    // if contract.cfdi is not null then sendError(res, 400, 'CFDIAlreadyExists', 'El contrato ya tiene CFDI')
-    // blob_id = insert into blobs (autoincrement(), req.headers['Content-Type'], req.body)
-    // update contracts where contract_id = :contract_id set cfdi_id = blob_id
-    // res.status(204).end
